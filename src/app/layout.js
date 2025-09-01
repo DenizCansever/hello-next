a145d017c25a8955df34d061f36486fc5eb4617a
Deniz Cansever <67337875+DenizCansever@users.noreply.github.com>
2025-09-01 14:36:14 +0300

    check layout for ssr

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,11 +3,14 @@ import React from 'react';
 import './styles.css';
 
 function RootLayout({ children }) {
+  console.log('RootLayout');
+  const timestamp = new Date().toLocaleString();
+
   return (
-    <html lang="en">
+    <html lang='en'>
       <body>
         {children}
-
+        <footer>Page rendered on {timestamp}</footer>
       </body>
     </html>
   );
